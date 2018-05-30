114bf4394fdb36999b5c5fa73210981d28ba54c2
Priyansh Jain <priyansh.jain0246@gmail.com>
2018-05-30 14:10:56 +0530

    Move files to respective folders

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -13,35 +13,35 @@ rm -fr config/*
 rm -fr crypto-config/*
 
 # generate crypto material
-cryptogen generate --config=./crypto-config.yaml
+cryptogen generate --config=../crypto-config.yaml
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate crypto material..."
   exit 1
 fi
 
 # generate genesis block for orderer
-configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./config/orderer.block
+configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ../config/orderer.block
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
   exit 1
 fi
 
 # generate channel configuration transaction
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ../config/channel.tx -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ../config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Org1MSP..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./config/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ../config/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Org2MSP..."
   exit 1
